59260947e3a1cc8e4b33ad6ebf741700d6ed3679
Martin Montes <martin11lrx@gmail.com>
2023-02-05 23:00:22 +0100

    Updated bump bundle script

--- hack/bump-bundle.sh
+++ hack/bump-bundle.sh
@@ -5,3 +5,20 @@ set -euo pipefail
 VERSION=$1
 
 echo "📦 Bumping bundle to version '$VERSION'"
+
+echo "📦 Updating Makefile"
+sed -i "s/VERSION ?= .*/VERSION ?= $VERSION/g" Makefile
+
+echo "📦 Generating bundle"
+make bundle
+
+echo "📦 Pushing changes"
+git config user.email "martin11lrx@gmail.com"
+git config user.name "Martin Montes"
+git add .
+git commit -m "Bump bundle to version '$VERSION'"
+git push
+
+echo "📦 Creating tag"
+git tag $VERSION
+git push --tags
